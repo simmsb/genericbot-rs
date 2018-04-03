b34fa1ff949c011535fe2e42a0b972a3497b3570
Ben Simms <ben@bensimms.moe>
2018-04-03 01:13:36 +0100

    Add: List tags and some stuff

--- src/commands/tags.rs
+++ src/commands/tags.rs
@@ -6,6 +6,7 @@ use serenity::{
         CommandError,
     },
 };
+use serenity;
 use diesel::prelude::*;
 use diesel;
 use ::PgConnectionManager;
@@ -14,7 +15,7 @@ use models::Tag;
 #[macro_use]
 use utils::macros::*;
 
-fn get_tag(ctx: &Context, g_id: i64, tag_key: &String) -> Option<Tag> {
+fn get_tag(ctx: &Context, g_id: i64, tag_key: &String) -> QueryResult<Tag> {
     use schema::tag::dsl::*;
 
     let data = ctx.data.lock();
@@ -23,7 +24,7 @@ fn get_tag(ctx: &Context, g_id: i64, tag_key: &String) -> Option<Tag> {
 
     tag.filter(guild_id.eq(&g_id))
        .filter(key.eq(tag_key))
-       .first(pool).ok()
+       .first(pool)
 }
 
 
@@ -56,7 +57,36 @@ fn delete_tag_do(ctx: &Context, tag_id: i64) {
     let pool = &*data.get::<PgConnectionManager>().unwrap().get().unwrap();
     drop(data);
 
-    diesel::delete(tag.filter(id.eq(tag_id))).execute(pool);
+    diesel::delete(tag.filter(id.eq(tag_id))).execute(pool).unwrap();
+}
+
+
+fn get_tags_range(ctx: &Context, g_id: i64, page: i64) -> QueryResult<Vec<Tag>> {
+    use schema::tag::dsl::*;
+
+    let data = ctx.data.lock();
+    let pool = &*data.get::<PgConnectionManager>().unwrap().get().unwrap();
+    drop(data);
+
+    let start = page * 20;
+    tag.filter(guild_id.eq(&g_id))
+       .order(key.asc())
+       .offset(start)
+       .limit(20)
+       .load(pool)
+}
+
+
+fn get_tag_count(ctx: &Context, g_id: i64) -> QueryResult<i64> {
+    use schema::tag::dsl::*;
+
+    let data = ctx.data.lock();
+    let pool = &*data.get::<PgConnectionManager>().unwrap().get().unwrap();
+    drop(data);
+
+    tag.filter(guild_id.eq(&g_id))
+       .count()
+       .get_result(pool)
 }
 
 
@@ -64,7 +94,7 @@ command!(add_tag(ctx, msg, args) {
     let key = get_arg!(args, single_quoted, String, key);
     let value = get_arg!(args, multiple, String, key).join(" ");
 
-    if let Some(t) = get_tag(&ctx, msg.guild_id().unwrap().0 as i64, &key) {
+    if let Ok(t) = get_tag(&ctx, msg.guild_id().unwrap().0 as i64, &key) {
         msg.channel_id.say(format!("The tag: {} already exists", t.key))?;
     } else if key.len() >= 50 {
         msg.channel_id.say("Tag keys cannot be longer than 50 characters.")?;
@@ -78,7 +108,7 @@ command!(add_tag(ctx, msg, args) {
 command!(tag(ctx, msg, args) {
     let key = get_arg!(args, multiple, String, key).join(" ");
 
-    if let Some(t) = get_tag(&ctx, msg.guild_id().unwrap().0 as i64, &key) {
+    if let Ok(t) = get_tag(&ctx, msg.guild_id().unwrap().0 as i64, &key) {
         msg.channel_id.say(t.text)?;
     } else {
         msg.channel_id.say("This tag does not exist.")?;
@@ -91,10 +121,11 @@ command!(delete_tag(ctx, msg, args) {
 
     let key = get_arg!(args, multiple, String, key).join(" ");
 
-    if let Some(t) = get_tag(&ctx, msg.guild_id().unwrap().0 as i64, &key) {
+    if let Ok(t) = get_tag(&ctx, msg.guild_id().unwrap().0 as i64, &key) {
 
         let cache = CACHE.read();
         let has_manage_messages = {
+            // TODO: refactor, use guild.member_permissions instead of getting the member
             if let Some(guild) = cache.guild(msg.guild_id().unwrap()) {
                 if let Some(member) = guild.read().members.get(&msg.author.id) {
                     member.permissions().ok().map_or(false, |p| p.manage_messages())
@@ -118,6 +149,61 @@ command!(delete_tag(ctx, msg, args) {
 });
 
 
+command!(list_tags(ctx, msg, args) {
+    use serenity::utils::{with_cache, MessageBuilder};
+    use std::cmp;
+
+    let page = args.single::<i64>().unwrap_or(1) - 1;
+
+    if page < 0 {
+        msg.channel_id.say(format!("That page does not exist."))?;
+        return Ok(());
+    }
+
+    let start = page * 20;
+    let last = (page + 1) * 20 - 1;
+    let tag_list: Vec<Tag> = get_tags_range(&ctx, msg.guild_id().unwrap().0 as i64, page as i64)?;
+    let tag_count = get_tag_count(&ctx, msg.guild_id().unwrap().0 as i64)?;
+
+    if start > tag_count {
+        msg.channel_id.say(format!("The requested page ({}) is greater than the number of pages ({}).", page, last / 20))?;
+        return Ok(());
+    }
+
+    let user_ids: Vec<i64> = tag_list.iter().map(|t| t.author_id).collect();
+
+    let user_names: Vec<String> = match with_cache( // AAAAAAAAAAAAAAAAa
+        |cache| cache.guild(msg.guild_id().unwrap()).map(|g| {
+            let members = &g.read().members;
+            user_ids.iter().map(
+                |&id| members.get(&From::from(id as u64)).map_or_else(
+                    || id.to_string(),
+                    |m| m.display_name().to_string()))
+                .collect()
+        })) {
+        Some(x) => x,
+        None    => user_ids.iter().map(|&id| id.to_string()).collect(),
+    };
+
+
+    let tag_content: String = user_names
+        .into_iter()
+        .zip(tag_list)
+        .enumerate().map(
+            |(i, (name, tag_v))|
+            format!("{:>3} | {}: {:.50}", i, name, tag_v.key)
+        ).collect::<Vec<String>>().join("\n");
+
+
+    let content = MessageBuilder::new()
+        .push_line(format!("Tags {}-{} of {}", start, cmp::min(last, tag_count), tag_count))
+        .push_codeblock_safe(tag_content, None)
+        .build();
+
+    msg.channel_id.say(content)?;
+});
+
+
 pub fn setup_tags(_client: &mut Client, frame: StandardFramework) -> StandardFramework {
     frame.group("Tags",
                 |g| g
@@ -128,13 +214,15 @@ pub fn setup_tags(_client: &mut Client, frame: StandardFramework) -> StandardFra
                         .desc("Create a tag with a name and response.")
                         .example("\"something\" This tag's content.")
                         .usage("{tag name} {tag content}")
+                        .num_args(2)
                 )
                 .command(
                     "tag", |c| c
                         .cmd(tag)
                         .desc("Retrieve a tag.")
                         .example("\"something\"")
-                        .usage("{tag_name}")
+                        .usage("{tag name}")
+                        .num_args(1)
                 )
                 .command(
                     "delete_tag", |c| c
@@ -142,6 +230,15 @@ pub fn setup_tags(_client: &mut Client, frame: StandardFramework) -> StandardFra
                         .desc("Delete a tag, only the owner of the tag, or a member with manage message perms can delete tags.")
                         .example("tag name")
                         .usage("{tag name}")
+                        .num_args(1)
+                )
+                .command(
+                    "list_tags", |c| c
+                        .cmd(list_tags)
+                        .desc("List tags for this guild.")
+                        .example("1 -- lists tags on the first page")
+                        .usage("{page}")
+                        .max_args(1)
                 )
     )
 }
