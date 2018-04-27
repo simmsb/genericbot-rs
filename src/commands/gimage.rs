6831c8d86326172f2d6c13a44c9092017a8bbb4b
Ben Simms <ben@bensimms.moe>
2018-04-27 13:15:53 +0100

    Add gimage command

--- src/commands/gimage.rs
+++ src/commands/gimage.rs
@@ -1,17 +1,14 @@
 use serenity::{
     prelude::*,
-    builder::CreateEmbed,
     framework::standard::{
         StandardFramework,
     },
 };
-use serde_json::Value;
-use serde_json;
 use reqwest::header;
 use reqwest;
-use itertools::Itertools;
 use rand::{Rng, thread_rng};
 use typemap::Key;
+use regex::Regex;
 
 struct ImageClient;
 
@@ -20,21 +17,61 @@ impl Key for ImageClient {
 }
 
 fn make_gimage_client() -> reqwest::Client {
-    // let mut headers = header::Headers::new();
+    let mut headers = header::Headers::new();
+    headers.set(header::UserAgent::new("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"));
     reqwest::Client::builder()
+        .default_headers(headers)
         .build()
         .unwrap()
 }
 
-struct ImageResponse {
-    url: String,
-}
+struct ImageResponse;
 
 impl ImageResponse {
-    fn search(client: &Client, value: &str) -> Self {
-        // TODO client.get()
+    fn params<'a>(search: &'a str) -> Vec<(&'static str, &'a str)> {
+        vec![("q", search), ("tbm", "isch"), ("safe", "high")]
+    }
+
+    fn search_for(client: &reqwest::Client, search: &str) -> Result<String, &'static str> {
+        let params = Self::params(search);
+        client.get("https://www.google.com/search")
+              .query(&params)
+              .send().map_err(|_| "No response from google")?
+              .text().map_err(|_| "Invalid response from google")
+    }
+
+    fn select_response(resp: String) -> Result<String, &'static str> {
+        lazy_static! {
+            static ref IMG_REGEX: Regex = Regex::new(r#""ou":"([^"]*)""#).unwrap();
+        }
+
+        let images: Vec<_> = IMG_REGEX.captures_iter(&resp).collect();
+        return thread_rng().choose(&images).map(|s| s.get(1).unwrap().as_str().to_owned()).ok_or("No images found");
+    }
+
+    fn search(client: &reqwest::Client, search: &str) -> Result<String, &'static str> {
+        let resp = Self::search_for(&client, search)?;
+        Self::select_response(resp)
     }
 }
+
+
+command!(gimage_cmd(ctx, msg, args) {
+    let search = args.full();
+
+    let client = {
+        let lock = ctx.data.lock();
+        lock.get::<ImageClient>().unwrap().clone()
+    };
+
+    let resp = ImageResponse::search(&client, &search)?;
+
+    void!(msg.channel_id.send_message(|m| m.embed(
+        |e| e.colour(0xaf38e4)
+             .title(format!("GImage response for {}", search))
+             .image(resp)
+    )));
+});
 
 
 pub fn setup_gimage(client: &mut Client, frame: StandardFramework) -> StandardFramework {
@@ -43,5 +80,13 @@ pub fn setup_gimage(client: &mut Client, frame: StandardFramework) -> StandardFr
         data.insert::<ImageClient>(make_gimage_client());
     }
 
-    frame
+    frame.group("GImage",
+                |g| g
+                .command("gimage", |c| c
+                         .cmd(gimage_cmd)
+                         .desc("Search google for images")
+                         .example("memes")
+                         .usage("{search string}")
+                )
+    )
 }
